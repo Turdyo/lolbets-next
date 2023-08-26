f496cd765d35e60aea907bcc5125756c1dcdc7a6
Turdyo <martin.gayet@efrei.net>
2023-08-26 02:21:18 +0200

    🔀 chore(route.ts): add dynamic variable to force dynamic import of the file 🐛 fix(route.ts): add newline at the end of the file to comply with coding standards

--- app/api/query/leaderboard/route.ts
+++ app/api/query/leaderboard/route.ts
@@ -1,6 +1,8 @@
 import { getLeaderboardUsers } from "@/lib/query";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     return NextResponse.json(await getLeaderboardUsers())
 }

103a7e7edfb8d91aff657d1230420fd536e39d26
Turdyo <martin.gayet@efrei.net>
2023-09-04 04:24:08 +0200

    🔧 chore(package.json): add postinstall script to generate Prisma client after installation 🔧 chore(package.json): remove solid-start-vercel dependency as it is no longer needed 🔧 chore(vite.config.ts): remove vercel adapter from solid-start plugin configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import solid from "solid-start/vite";
-import vercel from "solid-start-vercel"
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({
-    adapter: vercel({edge: false})
-  })],
+  plugins: [solid()],
 });

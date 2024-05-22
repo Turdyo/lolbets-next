fe5a1293e059a73446c31dfe3a1564b870ab9f63
Turdyo <martin.gayet@efrei.net>
2024-05-22 20:51:57 +0200

    i

--- src/app/api/bets/route.ts
+++ src/app/api/bets/route.ts
@@ -1,6 +1,9 @@
 import { distributeBets } from "@/lib/actions/bet"
 import { NextResponse } from "next/server"
 
+// Opt out of caching for all data requests in the route segment
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
 	console.log("distributing bets")
 	// const session = await auth()

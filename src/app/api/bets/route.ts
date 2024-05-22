133a28d6c949d0843e593f800cda7e78c397481d
Turdyo <martin.gayet@efrei.net>
2024-05-22 19:00:00 +0200

    i

--- src/app/api/bets/route.ts
+++ src/app/api/bets/route.ts
@@ -1,12 +1,11 @@
-import { auth } from "@/auth"
 import { distributeBets } from "@/lib/actions/bet"
 import { NextResponse } from "next/server"
 
 export async function GET() {
-	const session = await auth()
-	if (!session) {
-		return NextResponse.json({ error: "Must be logged in" })
-	}
+	// const session = await auth()
+	// if (!session) {
+	// 	return NextResponse.json({ error: "Must be logged in" })
+	// }
 	await distributeBets()
 	return NextResponse.json({ response: "done" })
 

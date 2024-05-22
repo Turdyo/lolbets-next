0f6943fcf0b41635e8748874ba9d9d695d33be3d
Turdyo <martin.gayet@efrei.net>
2024-05-22 19:00:29 +0200

    i

--- src/app/api/bets/route.ts
+++ src/app/api/bets/route.ts
@@ -2,11 +2,11 @@ import { distributeBets } from "@/lib/actions/bet"
 import { NextResponse } from "next/server"
 
 export async function GET() {
+	console.log("distributing bets")
 	// const session = await auth()
 	// if (!session) {
 	// 	return NextResponse.json({ error: "Must be logged in" })
 	// }
 	await distributeBets()
 	return NextResponse.json({ response: "done" })
-
 }
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -8,6 +8,8 @@ import { NextResponse } from "next/server"
 export const dynamic = "force-dynamic"
 
 export async function GET() {
+	console.log("updating matches")
+
 	const matches = await fetchMatches()
 
 	const leagues = matches.map((match) => match.league)
